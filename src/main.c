ec7e86bd510c62056104e0ddcb98a3e5522098ab
April <fynjyxbrjd@gmail.com>
2025-07-13 17:39:07 +0300

    add a placeholder server
    Prototype a dummy server (src/main.c), which binds an available address
    and listens for incoming connections, printing them to stdout and
    responding with `Connection: close`.

---
+++ src/main.c
@@ -0,0 +1,187 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <netdb.h>
+
+#define MAXDATASIZE 1024
+
+/*
+ * ai_ for AddrInfo
+ * gai_ for GetAddrInfo
+ * sin_ for Sockaddr_IN
+ * sa_ for SockAddr
+ * AF_ for Address Family
+ * PF_ for Protocol Family
+ */
+
+// prune dead forked processes
+void zombie_killer(int s) {
+	(void)s; // unused parameter warning
+
+	int saved_errno = errno; // errno may be overwritten during waitpid
+
+	while (waitpid(-1, NULL, WNOHANG) > 0);
+
+	errno = saved_errno;
+}
+
+// retrieve socket address (v4 or v6) from a generic sockaddr struct
+// cast to either sockaddr_in* or sockaddr_in6*
+void *get_sockaddr_in(struct sockaddr *sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in *)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+// bind the socket to the first available bindable address, returns the fd of
+// the socket or -1 on error
+int bind_local_address() {
+	struct addrinfo addr_hints, *server_info, *addr;
+	int listener_fd = -1;
+	int retval;
+	char addrstr[INET6_ADDRSTRLEN];
+
+	memset(&addr_hints, 0, sizeof addr_hints);
+	addr_hints.ai_family = AF_INET; // IPv4
+	addr_hints.ai_socktype = SOCK_STREAM; // TCP
+	addr_hints.ai_flags = AI_PASSIVE; // `bind()`able
+
+	retval = getaddrinfo(NULL, "http", &addr_hints, &server_info);
+	if (retval != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(retval));
+		return -1;
+	}
+
+	for (addr = server_info; addr != NULL; addr = addr->ai_next) {
+		listener_fd = socket(addr->ai_family,
+				addr->ai_socktype,
+				addr->ai_protocol);
+		if (listener_fd == -1) {
+			perror("socket");
+			continue;
+		}
+		int yes = 1;
+		retval = setsockopt(listener_fd,
+				SOL_SOCKET,
+				SO_REUSEADDR,
+				&yes, sizeof yes);
+		if (retval == -1) {
+			perror("setsockopt");
+			return -1;
+		}
+		retval = bind(listener_fd, addr->ai_addr, addr->ai_addrlen);
+		if (retval == -1) {
+			close(listener_fd);
+			perror("bind");
+			continue;
+		}
+		break;
+	}
+
+
+	if (listen(listener_fd, SOMAXCONN) == -1) {
+		perror("listen");
+		return 1;
+	}
+
+	inet_ntop(addr->ai_addr->sa_family,
+			get_sockaddr_in(addr->ai_addr),
+			addrstr, sizeof addrstr);
+
+	printf("server: listening on %s...\n", addrstr);
+
+	freeaddrinfo(server_info);
+
+	return listener_fd;
+}
+
+void handle_client(int client_fd) {
+	char buf[MAXDATASIZE];
+
+	int num_bytes = recv(client_fd, buf, MAXDATASIZE - 1, 0);
+	if (num_bytes == -1) {
+		perror("recv");
+		exit(1);
+	}
+
+	buf[num_bytes] = '\0';
+	printf("server: message received, contents below\n%s\n", buf);
+
+	const char *reply =
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Length: 0\r\n"
+		"Connection: close\r\n"
+		"\r\n";
+	send(client_fd, reply, strlen(reply), 0);
+}
+
+int main(void) {
+	int listener_fd, client_fd;
+	struct sockaddr_storage client_addr;
+	struct sigaction sigact;
+	socklen_t sin_size;
+	char addrstr[INET6_ADDRSTRLEN];
+
+	listener_fd = bind_local_address();
+
+	if (listener_fd == -1) {
+		fprintf(stderr, "server: failed to bind\n");
+		close(listener_fd);
+		return 1;
+	}
+
+	sigact.sa_handler = zombie_killer;
+	sigemptyset(&sigact.sa_mask);
+	sigact.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sigact, NULL) == -1) {
+		perror("sigaction");
+		close(listener_fd);
+		return 1;
+	}
+
+	while (1) {
+		sin_size = sizeof client_addr;
+		client_fd = accept(listener_fd,
+				(struct sockaddr*)&client_addr,
+				&sin_size);
+
+		if (client_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(client_addr.ss_family,
+				get_sockaddr_in((struct sockaddr*)&client_addr),
+				addrstr, sizeof addrstr);
+		printf("server: received connection from %s\n", addrstr);
+
+		pid_t pid = fork();
+
+		if (pid == -1) {
+			perror("fork");
+			close(listener_fd);
+			return 1;
+		}
+
+		if (!pid) { // child
+			// child inherited a copy of parent's file descriptors
+			// sockfd is passive and isn't used by the child
+			close(listener_fd);
+
+			handle_client(client_fd);
+			close(client_fd);
+
+			return 0;
+		} // parent
+		close(client_fd);
+	}
+	return 0;
+}
