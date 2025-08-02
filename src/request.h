0a11751d30455083fe44108189c0221c7d7fee6d
April <fynjyxbrjd@gmail.com>
2025-08-02 17:35:41 +0300

    fill `struct`s and `enum`s for the request parsing

---
+++ src/request.h
@@ -0,0 +1,87 @@
+#include <stddef.h>
+#include <stdint.h>
+
+struct slice {
+	const char *ptr;
+	size_t len;
+};
+
+enum http_header_type {
+	HH_UNK = 0,
+	HH_ACCEPT,
+	HH_ACCEPT_CHARSET,
+	HH_ACCEPT_ENCODING,
+	HH_ACCEPT_LANGUAGE,
+	HH_AUTHORIZATION,
+	HH_CACHE_CONTROL,
+	HH_CONNECTION,
+	HH_CONTENT_ENCODING,
+	HH_CONTENT_LANGUAGE,
+	HH_CONTENT_LENGTH,
+	HH_CONTENT_LOCATION,
+	HH_CONTENT_MD5,
+	HH_CONTENT_RANGE,
+	HH_CONTENT_TYPE,
+	HH_DATE,
+	HH_EXPECT,
+	HH_EXPIRES,
+	HH_FROM,
+	HH_HOST,
+	HH_IF_MATCH,
+	HH_IF_MODIFIED_SINCE,
+	HH_IF_NONE_MATCH,
+	HH_IF_RANGE,
+	HH_IF_UNMODIFIED_SINCE,
+	HH_MAX_FORWARDS,
+	HH_PRAGMA,
+	HH_PROXY_AUTHORIZATION,
+	HH_RANGE,
+	HH_REFERER,
+	HH_TE,
+	HH_TRAILER,
+	HH_TRANSFER_ENCODING,
+	HH_UPGRADE,
+	HH_USER_AGENT,
+	HH_VARY,
+	HH_VIA,
+	HH_WARNING,
+};
+
+struct http_header {
+	struct slice name;
+	struct slice value;
+	enum http_header_type type;
+};
+
+enum http_method {
+	HM_UNK = 0,
+	HM_GET,
+	HM_HEAD,
+	HM_POST,
+	HM_PUT,
+	HM_DELETE,
+	HM_CONNECT,
+	HM_OPTIONS,
+	HM_TRACE
+};
+
+enum request_target_form {
+	TF_ORIGIN,
+	TF_ABSOLUTE,
+	TF_AUTHORITY,
+	TF_ASTERISK
+};
+
+struct http_request {
+	enum http_method method;
+
+	uint8_t http_major, http_minor;
+
+	enum request_target_form target_form;
+	struct slice raw_target;
+
+	struct http_header *headers;
+	size_t num_headers, cap_headers;
+
+	void *body_ctx;
+};
